01dae02576eb258da1599955eac91416139ca383
haydenleedev <haydenlee22@gmail.com>
2021-09-02 03:54:42 -0400

    added layout styles

--- pages/assets.js
+++ pages/assets.js
@@ -1,5 +1,7 @@
 import * as svgComponent from 'components/icons';
 import Head from 'next/head';
+import layout from 'styles/layout.module.scss'
+import nav from 'styles/nav.module.scss'
 import icons from 'styles/buttons.module.scss';
 import styles from 'styles/packages.module.scss';
 
@@ -14,71 +16,37 @@ export default function Assets() {
       </Head>
 
       <h1 className={styles.title}>Hello UJET!</h1>
-      <div className={styles.clearfix}>
-        <button
-          className={`${icons['btn']} ${icons['btn-white']} ${icons['btn-big']}`}
-        >
-          <span>Back</span>
-        </button>
-        <button
-          className={`${icons['btn']} ${icons['btn-orange']} ${icons['btn-big']}`}
-        >
-          <span>Continue</span>
-        </button>
-        <button
-          className={`${icons['btn']} ${icons['btn-blue']} ${icons['btn-big']}`}
-        >
-          <span>Buy Now</span>
-        </button>
-        <button
-          className={`${icons['btn']} ${icons['btn-blue']} ${icons['btn-small']}`}
-        >
-          <span>Try Now</span>
-        </button>
-        <button
-          className={`${icons['btn']} ${icons['btn-skyblue']} ${icons['btn-medium']}`}
-        >
-          <span>+ Compare all plans and features</span>
-        </button>
-      </div>
+        <section>
+          <button className={`${icons['btn']} ${icons['btn-white']} ${icons['btn-big']}` }><span>Back</span></button>
+          <button className={`${icons['btn']} ${icons['btn-orange']} ${icons['btn-big']}` }><span>Continue</span></button>
+          <button className={`${icons['btn']} ${icons['btn-blue']} ${icons['btn-big']}` }><span>Buy Now</span></button>
+          <button className={`${icons['btn']} ${icons['btn-blue']} ${icons['btn-small']}` }><span>Try Now</span></button>
+          <button className={`${icons['btn']} ${icons['btn-skyblue']} ${icons['btn-medium']}` }><span>+ Compare all plans and features</span></button>
+        </section>
 
-      <div
-        className={`${styles['grid-col-4']} ${styles['pt-30px']} ${styles['pb-30px']}`}
-      >
-        <div className={styles.relative}>
-          <span>All Core Channels*</span>
-          <span className={icons['tooltip-wrap']}>
-            <span className={icons['tooltip']}>
-              Support for inbound calls over the PSTN as well as outbound
-              calling via the agent dialpad or click-to-call within the CRM.
-            </span>
-          </span>
+        <div className={`${nav['grid-col-4']} ${styles['pt-30px']} ${styles['pb-30px']}`}>
+            <div className={styles.relative}>
+              <span>All Core Channels*</span>
+                <span className={icons['tooltip-wrap']}>
+                  <span className={icons['tooltip']}>
+                          Support for inbound calls over the PSTN as well as outbound calling via the agent dialpad or click-to-call within the CRM.</span>
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.clearfix}><svgComponent.UjetCxLogo/></div>
+          <div><svgComponent.IconX/></div>
+          <div><svgComponent.IconCheck/></div>
+          <div><svgComponent.IconCheckCircle/></div>
+          <span className={icons.circle}></span>
+
+          <div className={styles.relative}><span className={icons['radio-btn']}></span></div>
+
+          <div className={styles['table-headline-gray']}>Includes</div>   
+          <div className={styles['table-headline-blue']}>Today's Charge</div>
+          <div className={layout.grid}>
+          <span className={`${styles.number} ${layout['align-center']}`}>1</span> 
         </div>
-      </div>
-
-      <div className={styles.clearfix}>
-        <svgComponent.UjetCxLogo />
-      </div>
-      <div>
-        <svgComponent.IconX />
-      </div>
-      <div>
-        <svgComponent.IconCheck />
-      </div>
-      <div>
-        <svgComponent.IconCheckCircle />
-      </div>
-      <span className={icons.circle}></span>
-
-      <div className={styles.relative}>
-        <span className={icons['radio-btn']}></span>
-      </div>
-
-      <div className={styles['table-headline-gray']}>Includes</div>
-      <div className={styles['table-headline-blue']}>Today's Charge</div>
-      <div className={styles.grid}>
-        <span className={`${styles.number} ${styles['align-center']}`}>1</span>
-      </div>
     </div>
   );
 }
